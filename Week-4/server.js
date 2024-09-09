038ef3e843db3afa1891fa51e073a1991e9d3ab1
amyadavv <amityadav010213@gmail.com>
2024-09-09 12:10:51 +0530

    finally connecting frontend and backend

---
+++ Week-4/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+//we use cors because Cross-Origin Resource Sharing (CORS): 
+// If your frontend and backend are running on different ports 
+// (e.g., frontend on localhost:5500 and backend on localhost:3000),
+//  you'll need to enable CORS on your backend to allow requests from your 
+//  frontend.
+
+app.get("/ans", function (req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const ans = a + b;
+    res.send(ans.toString());
+
+})
+
+app.listen(port, function () {
+    console.log('running');
+});
